41fb31b6915c0ca753cbe2342f41effe69a712ea
Saran <saranmech886@gmail.com>
2025-04-24 10:59:02 +0530

    fix:

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import { HeroSection } from "@/components/hero-section"
-import { ServicesSection } from "@/components/services-section"
-import { TestimonialsSection } from "@/components/testimonials-section"
-import { CTASection } from "@/components/cta-section"
-
+import { HeroSection } from "../components/hero-section"
+import { ServicesSection } from "../components/services-section"
+import { TestimonialsSection } from "../components/testimonials-section"
+import { CTASection } from "../components/cta-section"
+//somthying
 export default function Home() {
   return (
     <div className="flex flex-col gap-16 pb-8">
